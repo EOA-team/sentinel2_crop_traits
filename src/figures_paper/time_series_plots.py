0515ff3ceeb6d96464f9b0eb1e5c7a828d31a770
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-05-02 16:21:20 +0200

    trying to plot all three traits

--- src/figures_paper/time_series_plots.py
+++ src/figures_paper/time_series_plots.py
@@ -14,10 +14,11 @@ import seaborn as sns
 
 from eodal.core.raster import RasterCollection
 from pathlib import Path
+from typing import List
 
 from utils import TraitLimits
 
-mpl.rc('font', size=16)
+mpl.rc('font', size=20)
 plt.style.use('bmh')
 
 
@@ -26,6 +27,7 @@ def get_parcel_ts(
     parcel_name: str,
     parcel_gdf: gpd.GeoDataFrame,
     img_dir: Path,
+    traits: List[str]
 ) -> pd.DataFrame:
     """
     Function to retrieve the median trait time series per parcel.
@@ -45,60 +47,66 @@ def get_parcel_ts(
     :returns:
         DataFrame with median trait time series per parcel.
     """
-    # loop over traits
-    for _, trait in enumerate(traits):
 
-        inv_res_insitu_points = pd.read_csv(
-            trait_settings[trait]['trait_data'])
-        inv_res_farm = inv_res_insitu_points[
-            (inv_res_insitu_points.location == farm) &
-            (inv_res_insitu_points.parcel == parcel_name)
-        ].copy()
-        # loop over Sentinel-2 scenes and save median, q05, q95 of the data
-        ts_list = []
-        for s2_scene in img_dir.glob('S2*.SAFE'):
-            scene_id = s2_scene.name
-            inv_res_scene = inv_res_farm[
-                inv_res_farm.scene_id == scene_id].copy()
-            if inv_res_scene.empty:
-                continue
-            # select the corresponding model output based on the BBCH rating
-            bbch_stage = inv_res_scene["Macro-Stage"].iloc[0].replace(' ', '')
-            try:
-                agdd = int(np.round(inv_res_scene.gdd_cumsum.iloc[0]))
-            except IndexError:
-                print('aarg')
-            fpath_inv_img = s2_scene.joinpath(
-                f'{bbch_stage}_lutinv_traits.tiff')
-            parcel_gdf_buffered = parcel_gdf.to_crs(epsg=2056).buffer(-20)
-            trait_ds = RasterCollection.from_multi_band_raster(
-                fpath_raster=fpath_inv_img,
-                vector_features=parcel_gdf_buffered
-            )
-            ts_list.append({
-                'q05': np.nanquantile(trait_ds[trait].values.data, 0.05),
-                'q50': np.nanquantile(trait_ds[trait].values.data, 0.5),
-                'q95': np.nanquantile(trait_ds[trait].values.data, 0.95),
-                'sensing_date': pd.to_datetime(
-                    scene_id.split('_')[2][0:8]).date(),
-                'agdd': agdd
-            })
+    inv_res_insitu_points = pd.read_csv(
+        trait_settings['lai']['trait_data'])
+    inv_res_farm = inv_res_insitu_points[
+        (inv_res_insitu_points.location == farm) &
+        (inv_res_insitu_points.parcel == parcel_name)
+    ].copy()
+    # loop over Sentinel-2 scenes and save median, q05, q95 of the data
+    ts_list = []
+    for s2_scene in img_dir.glob('S2*.SAFE'):
+        scene_id = s2_scene.name
+        inv_res_scene = inv_res_farm[
+            inv_res_farm.scene_id == scene_id].copy()
+        if inv_res_scene.empty:
+            continue
+        # select the corresponding model output based on the BBCH rating
+        bbch_stage = inv_res_scene["Macro-Stage"].iloc[0].replace(' ', '')
+        try:
+            agdd = int(np.round(inv_res_scene.gdd_cumsum.iloc[0]))
+        except IndexError:
+            print('aarg')
+        fpath_inv_img = s2_scene.joinpath(
+            f'{bbch_stage}_lutinv_traits.tiff')
+        parcel_gdf_buffered = parcel_gdf.to_crs(epsg=2056).buffer(-20)
+        trait_ds = RasterCollection.from_multi_band_raster(
+            fpath_raster=fpath_inv_img,
+            vector_features=parcel_gdf_buffered
+        )
+        # loop over traits
+        res_dict = {}
+        for _, trait in enumerate(traits):
+            res_dict.update({
+                f'{trait}_q05': np.nanquantile(trait_ds[trait].values.data, 0.05),
+                f'{trait}_q50': np.nanquantile(trait_ds[trait].values.data, 0.5),
+                f'{trait}_q95': np.nanquantile(trait_ds[trait].values.data, 0.95)})
 
-        ts_df = pd.DataFrame(ts_list)
-        ts_df.sort_values(by='agdd', inplace=True)
-        ts_df['farm'] = farm
-        ts_df['parcel'] = parcel_name
-        return ts_df
+        res_dict.update({
+            'sensing_date': pd.to_datetime(scene_id.split('_')[2][0:8]).date(),
+            'agdd': agdd})
+        ts_list.append(res_dict)
+
+    ts_df = pd.DataFrame(ts_list)
+    ts_df.sort_values(by='agdd', inplace=True)
+    ts_df['farm'] = farm
+    ts_df['parcel'] = parcel_name
+    return ts_df
 
 
 if __name__ == '__main__':
+
+    import os
+    cwd = Path(__file__).parent.absolute()
+    os.chdir(cwd)
 
     # in-situ measurements and inversion results of traits
     data_dir = Path('../../results/lut_based_inversion')
     # data_dir = Path('/mnt/ides/Lukas/04_Work/lut_based_inversion')
     out_dir = Path('../../results/Figures')
 
-    traits = ['lai']
+    traits = ['lai', 'ccc']
     trait_settings = {
         'lai': {
             'trait_name': 'Green Leaf Area Index',
@@ -129,7 +137,8 @@ if __name__ == '__main__':
             fpath_parcel_shp = farm_data_dir.joinpath(
                 farm).joinpath(f'{parcel_name}.shp')
             parcel_gdf = gpd.read_file(fpath_parcel_shp)
-            parcel_ts = get_parcel_ts(farm, parcel_name, parcel_gdf, img_dir)
+            parcel_ts = get_parcel_ts(
+                farm, parcel_name, parcel_gdf, img_dir, traits=['lai', 'ccc'])
             # save variety information
             parcel_ts['variety'] = parcel_df['Variety'].values[0]
             data_list.append(parcel_ts)
@@ -138,47 +147,49 @@ if __name__ == '__main__':
     df.index = [x for x in range(df.shape[0])]
     df['sensing_date'] = pd.to_datetime(df.sensing_date, format='%Y-%m-%d')
 
-    # plot time series of the single parcels into one figure
-    f, ax = plt.subplots(figsize=(25, 10), ncols=2, sharey=True)
-    sns.lineplot(
-        x='sensing_date',
-        y='q50',
-        hue='parcel',
-        data=df,
-        ax=ax[0],
-        legend=False,
-        marker='x')
-    sns.lineplot(
-        x='agdd', y='q50', hue='parcel', data=df, ax=ax[1], marker='x')
-    ax[0].set_xlabel('Date (YYYY-MM)')
-    ax[0].set_ylabel(r'Median GLAI [$m^2$ $m^{-2}$]')
-    ax[0].set_title('(a) Calendar Dates')
-    plt.setp(ax[0].xaxis.get_majorticklabels(), rotation=45)
-    ax[1].set_title('(b) Thermal Time')
-    ax[1].set_xlabel(r'Accumulated Growing Degree Days [$deg$ $C$]')
-    plt.setp(ax[1].xaxis.get_majorticklabels(), rotation=45)
-    f.savefig(out_dir.joinpath('ts_dates_agdds.png'))
+    # calculate the leaf chlorophyll content
+    df['cab_q50'] = df['ccc_q50'] / df['lai_q50'] * 100
 
-    # plot time series of the varieties into one plot
-    f, ax = plt.subplots(figsize=(25, 10), ncols=2, sharey=True)
-    sns.lineplot(
-        x='sensing_date',
-        y='q50',
-        hue='variety',
-        data=df,
-        ax=ax[0],
-        legend=False,
-        marker='x')
-    sns.lineplot(
-        x='agdd', y='q50', hue='variety', data=df, ax=ax[1], marker='x')
-    ax[0].set_xlabel('')
-    ax[0].set_ylabel(r'Median GLAI [$m^2$ $m^{-2}$]')
-    ax[0].set_title('Calendar Dates')
-    plt.setp(ax[0].xaxis.get_majorticklabels(), rotation=45)
-    ax[1].set_title('AGDDs')
-    ax[1].set_xlabel('')
-    plt.setp(ax[1].xaxis.get_majorticklabels(), rotation=45)
-    f.savefig(out_dir.joinpath('ts_dates_agdds_variety.png'))
+    # plot time series of the single parcels into one figure
+    f, ax = plt.subplots(figsize=(30, 10), ncols=2, nrows=3)
+    ax = ax.flatten()
+
+    traits_to_plot = ['lai', 'ccc', 'cab']
+    traits_to_plot_names = ['GLAI', 'CCC', 'Cab']
+    units = [r'[$m^2$ $m^{-2}$]', r'[$g$ $m^{-2}$]', r'[$\mu g$ $cm^{-2}$]']
+    for idx in range(3):
+        legend = False
+        if idx == 2:
+            legend = True
+        sns.lineplot(
+            x='sensing_date',
+            y=f'{traits_to_plot[idx]}_q50',
+            hue='parcel',
+            data=df,
+            ax=ax[idx],
+            legend=False,
+            marker='x')
+        sns.lineplot(
+            x='agdd',
+            y=f'{traits_to_plot[idx]}_q50',
+            hue='parcel',
+            data=df,
+            ax=ax[idx+1],
+            marker='x',
+            legend=legend)
+
+        ax[idx].set_ylabel(f'Median {traits_to_plot_names[idx]}' + units[idx])
+        ax[idx+1].set_ylabel('')
+        if idx == 0:
+            ax[idx].set_title('(a) Calendar Dates')
+            ax[idx+1].set_title('(b) Thermal Time')
+        if idx == 2:
+            ax[idx].set_xlabel('Date (YYYY-MM)')
+            ax[idx+1].set_xlabel(r'Accumulated Growing Degree Days [$deg$ $C$]')
+            plt.setp(ax[idx].xaxis.get_majorticklabels(), rotation=45)
+            plt.setp(ax[idx+1].xaxis.get_majorticklabels(), rotation=45)
+
+    f.savefig(out_dir.joinpath('ts_dates_agdds.png'))
 
     # save dataframe
     df.to_csv(out_dir.joinpath('ts_dates_agdds.csv'))
